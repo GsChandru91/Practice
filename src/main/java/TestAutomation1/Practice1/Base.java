2020f344cfbbe954936f5de59807bf68386916e2
Gschandru91 <gschandru91@gmail.com>
2022-07-09 17:19:43 +0530

    Pratice

---
+++ src/main/java/TestAutomation1/Practice1/Base.java
@@ -0,0 +1,15 @@
+package TestAutomation1.Practice1;
+
+import java.io.FileInputStream;
+import java.util.Properties;
+
+public class Base {
+
+	public void initializeDriver() {
+		
+		Properties prop = new Properties();
+		FileInputStream file = new FileInputStream("prop");
+		
+	}
+
+}
